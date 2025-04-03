4c623d0956dd82118282c920951b2e065ae69d91
Varun Kaladharan <108853879+varun-kaladharan1995@users.noreply.github.com>
2025-04-03 07:17:28 +0530

    Time: 1 ms (36.92%), Space: 54.3 MB (18.55%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+
+    const N = nums.length;
+    let l = 0, m = 0; h = N-1;
+
+    while(m<=h) {
+        if(nums[m] === 0) {
+            [nums[l], nums[m]] = [nums[m], nums[l]];
+            l++; m++;
+        } else if (nums[m] === 2) {
+            [nums[m], nums[h]] = [nums[h], nums[m]]; 
+            h--;
+        } else {
+            m++;
+        }
+    }
+    
+};
