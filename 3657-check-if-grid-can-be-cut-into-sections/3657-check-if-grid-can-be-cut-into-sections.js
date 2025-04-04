c588979ea3b11bb0e3ba28cca0b62af57b0a5bd5
Varun Kaladharan <108853879+varun-kaladharan1995@users.noreply.github.com>
2025-04-04 16:28:08 +0530

    Time: 164 ms (87.05%), Space: 94.4 MB (92.02%) - LeetHub

---
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @param {number[][]} rectangles
+ * @return {boolean}
+ */
+var checkValidCuts = function(n, rectangles) {
+    return canCut(rectangles, 0) || canCut(rectangles, 1);
+};
+
+function canCut(rectangles, axis) {
+    rectangles.sort((a,b) => a[axis] - b[axis]);
+    let cuts = 0, previousEnd = -1;
+
+    for(let rect of rectangles) {
+        let start = rect[axis], end = rect[axis + 2];
+
+        if(start >= previousEnd) cuts++;
+        previousEnd = Math.max(end, previousEnd);
+        if(cuts >= 3) return true;
+    }
+
+    return false;
+}
